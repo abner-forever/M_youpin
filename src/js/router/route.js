2aca74b4ca3bdf8cfbf26a5ba30309f59a853001
foreverheart <1661287843@qq.com>
2018-10-21 22:37:23 +0800

    change null

--- src/js/router/route.js
+++ src/js/router/route.js
@@ -4,13 +4,9 @@
 import home_controller from '../controllers/home_controller';
 import class_controller from '../controllers/class_controller';
 
-
-
 const routes = {
-
     "#/home": home_controller,
     "#/class": class_controller
-
 }
 
 export {
