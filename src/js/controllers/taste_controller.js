97d4c4d7e3b241fe2aca3d1dd18fa261cfd6da22
star <1570501382@qq.com>
2018-10-22 10:01:26 +0800

    swiper is ok

--- src/js/controllers/taste_controller.js
+++ src/js/controllers/taste_controller.js
@@ -3,6 +3,9 @@ import taste_template from "../views/taste.html";
 import smallbox from './small__box-box'
 import findgood from './findgood--many--father'
 import picturetemplate from './template'
+
+
+// import from '../../static/js/swiper-4.3.5.min.js'
 // import swiper from './swiper'
 
 
@@ -20,7 +23,18 @@ const render = () => {
     picturetemplate.picturetemplate();
     document.querySelector("#app").innerHTML = taste_template;
     // swiper.swiper();
-    
+    new Swiper('.swiper--big-box-father', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        // freeMode: true,
+        centeredSlides: true,
+        direction: 'horizontal', // 水平切换选项
+        loop: true,// 循环模式选项
+        autoplay: {
+            disableOnInteraction: false
+        }// 自动播放
+    })
+    console.log("swiper")
 
 
 }
