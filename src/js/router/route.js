78afc56d825c2a489c8df0062b97e2cd24c5b9e2
star <1570501382@qq.com>
2018-10-22 08:08:43 +0800

    is ok?

--- src/js/router/route.js
+++ src/js/router/route.js
@@ -6,10 +6,20 @@ import class_controller from '../controllers/class_controller';
 
 
 
+import ucenter_controller from '../controllers/ucenter_controller';
+import taste_controller from '../controllers/taste_controller';
+
+
+
 const routes = {
 
     "#/home": home_controller,
-    "#/class": class_controller
+    "#/class": class_controller,
+
+
+
+    "#/taste":taste_controller,
+    "#/ucenter":ucenter_controller
 
 }
 
